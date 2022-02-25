c93cc62414f0df9ec929f8d9102d6a0bebb1b44c
Sanjeet Mistry <sanjeetm@axioned.com>
2022-02-25 15:59:15 +0530

    Add 'UsersList' class - Checks if Logged in User is an admin - Displays all the users in DB - Update & Delete an User from DB

---
+++ utility/users-list.php
@@ -0,0 +1,90 @@
+<?php
+
+class UsersList 
+{
+    public static $numOfUsers;
+    public static $loggedInUserIsAdmin;
+
+    public static function createUserList() 
+    {
+        self::$loggedInUserIsAdmin = self::checkIfLoggedInUserIsAnAdmin();
+
+        DatabaseConnection::startConnection();
+        $select = mysqli_query(DatabaseConnection::$conn, "SELECT name, email, mobile, gender, city, admin FROM users;");
+
+        self::$numOfUsers = mysqli_num_rows($select);
+
+        echo "<li>";
+        echo "<span class='name'>Name</span>";
+        echo "<span class='email'>Email Id</span>";
+        echo "<span class='mobile'>Mobile No.</span>";
+        echo "<span class='gender'>Gender</span>";
+        echo "<span class='city'>City</span>";
+
+        if(self::$loggedInUserIsAdmin) {
+            echo "<span class='update'>Update User</span>";
+            echo "<span class='delete'>Delete User</span>";
+        }
+        echo "</li>";
+
+        while($row = mysqli_fetch_assoc($select)) {
+            echo "<li>";
+            echo "<span class='name'>" . $row["name"] . "</span>";
+            echo "<span class='email'>" . $row["email"] . "</span>";
+            echo "<span class='mobile'>" . $row["mobile"] . "</span>";
+            echo "<span class='gender'>" . $row["gender"] . "</span>";
+            echo "<span class='city'>" . $row["city"] . "</span>";
+            if(self::$loggedInUserIsAdmin) {
+                echo "<span class='update'><a href='" . constant('URL') . "/users.php?update=" . $row['email'] . "' title='Update'>Update</a></span>";
+                echo "<span class='delete'><a href='" . constant('URL') . "/users.php?email=" . $row['email'] . "' title='Delete'>Delete</a></span>";
+            }
+            echo "</li>";
+        }
+        DatabaseConnection::closeDBConnection();
+    }
+
+    public static function deleteUserFromDB() 
+    {
+        $email = $_GET["email"];
+        DatabaseConnection::startConnection();
+        // $delete = mysqli_query(DatabaseConnection::$conn, "delete from users where email = '$email'");
+
+        $stmt = DatabaseConnection::$conn->prepare("delete from users where email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->close();
+        DatabaseConnection::closeDBConnection();
+        
+        if($email == $_COOKIE[EMAIL]) {
+            setcookie(EMAIL, "", time() - 300, "/", "", 0);
+            header('Location: ' . URL . '/login.php');
+        }
+    }
+
+    public static function updateUserInDB() 
+    {
+        $email = $_GET["update"];
+
+        setcookie(UPDATE, $email, time() + 24 * 60 * 60, "/", "", 0);
+        header('Location: ' . URL);
+    }
+
+    public static function checkIfLoggedInUserIsAnAdmin() {
+        $email = $_COOKIE[EMAIL];
+        DatabaseConnection::startConnection();
+
+        $stmt = DatabaseConnection::$conn->prepare("SELECT admin FROM users where email=?;");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $rows = $stmt->num_rows;
+
+        $stmt->close();
+        DatabaseConnection::closeDBConnection();
+
+        if($rows >= 1) {
+            return true;
+        }
+        return false;
+    }
+}
